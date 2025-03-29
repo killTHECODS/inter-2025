ee2e32746950e65be8fd48aab86bb56444b692a9
Lamberto Maia <lambertomaia1@gmail.com>
2025-03-29 19:54:22 -0300

    Update authMiddleware.js

--- inter 2025/middlewares/authMiddleware.js
+++ inter 2025/middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 module.exports = (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) return res.status(401).json({ msg: 'Acesso não autorizado' });
-
+ALOALo
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
